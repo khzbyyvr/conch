06a93494b1d1bdb597af3969d239fe2da77a2506
khzbyyvr <khzbyyvr.code@fastmail.com>
2022-08-13 23:58:36 +1000

    Update main files

--- lua/conch/fetch.lua
+++ lua/conch/fetch.lua
@@ -1,18 +1,18 @@
-
+-- Imports the module for handling SQLite.
 local sqlite = require("ljsqlite3")
 
-local M {}
+-- Creates an object for the module.
+local M = {}
 
+-- Fetches todo tasks from the database and
+-- prints the output.
 function M.fetch_todos()
-	local db = sqlite.open("todo.db")
+    local db = sqlite.open("todo.db")
 
-	local db_results = db- exec("select * from todo_list where completed == 'No';")
-	for _, item in ipairs(db_results[2]) do print(item) end
+    local db_results = db:exec("SELECT * FROM todo_list WHERE completed == 'No';")
+    for _, item in ipairs(db_results[2]) do print(item) end
 
-	db-close()
+    db:close()
 end
 
 return M
-
-
-
